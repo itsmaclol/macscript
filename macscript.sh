88407f85e2eb9273d99df6a787b20429244c65c4
Mac <130684812+itsmaclol@users.noreply.github.com>
2023-05-13 15:04:43 +0200

    ill fix when i get home

--- macscript.sh
+++ macscript.sh
@@ -453,11 +453,7 @@ function main_menu {
     if [ "${macos_version:0:2}" == "20" ] || [ "${macos_version:0:2}" == "22" ]; then
         printf "│ %-2s│ %-30s │\n" "4" "Install Procursus (macOS only)"
     fi
-    if [ "${macos_version:0:2}" == "20" ] || [ "${macos_version:0:2}" == "22" ]; then
-        printf "│ %-2s│ %-30s │\n" "5" "Exit"
-    else
-        printf "│ %-2s│ %-30s │\n" "4" "Exit"
-    fi
+    printf "│ %-2s│ %-30s │\n" "5" "Exit"
     echo "└───┴────────────────────────────────┘"
 
     # Ask the user for input
